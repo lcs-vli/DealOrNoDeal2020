1c6298e0021fa95db153aba94356c7789043ff09
Victoria Li <VLi@lcs.on.ca>
2020-02-06 18:54:03 -0500

    thought the code would ask for the number of cases opened but if enter 2, then two lines appear together. Can only enter value for the second line and then there would be a error message

--- DealOrNoDeal/main.swift
+++ DealOrNoDeal/main.swift
@@ -29,7 +29,7 @@ while true {
         
         // Could not make input into an integer, so return to top and ask again
         continue
-
+        
     }
     
     // Check that integer is in desired range
@@ -71,9 +71,44 @@ var briefcaseValues = [100, 500, 1_000, 5_000, 10_000, 25_000, 50_000, 100_000, 
 //
 // An integer between 1 and 10, inclusive
 func getBriefcaseOpened(onTurn turn: Int) -> Int {
-    
-    // STUDENTS: Complete this function
-    
+   
+    while true{
+        
+        //ask for input
+        for briefcases in 1...briefcasesOpened{
+            print("What is the number of the \(briefcases) briefcase opened?")
+        }
+        
+        guard let briefcasesNumber = readLine() else {
+            
+            // No input given, return to top of loop and ask again
+            continue
+        }
+        
+        // Attempt to make input into an integer
+        guard let number = Int(briefcasesNumber) else {
+            
+            // Could not make input into an integer, so return to top and ask again
+            continue
+            
+        }
+        
+        // Check that integer is in desired range
+        // REMEMBER: Guard statement conditions describe what we WANT
+        guard number > 0, number < 11 else {
+            
+            // Integer not in desired range, return to top and ask again
+            continue
+            
+        }
+        
+        // If we've made it here, the input is valid
+        briefcaseValues[number]=0
+        
+        // Stop looping
+        break
+    }
+
     
     // The statement below can be modified
     return 0
@@ -101,8 +136,8 @@ for turn in 1...briefcasesOpened {
 //
 
 /* STUDENTS: Now tell the player whether to take the deal, or not.
-1. One who uses or makes use of something, a consumer.
-2. A person who uses drugs, especially illegal drugs.
-3. computing A person who uses a computer or a computing network, especially a person who has received a user account.
+ 1. One who uses or makes use of something, a consumer.
+ 2. A person who uses drugs, especially illegal drugs.
+ 3. computing A person who uses a computer or a computing network, especially a person who has received a user account.
  Definitions by Grammarly
-*/
+ */
